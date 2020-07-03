d4498cf68806190c6698edbf75e363a660194b09
Michael Koch <m.koch@know-how-akademie.de>
2020-07-03 07:37:04 +0200

    Gleitkommazahlen (float) können berechnet werden.

--- Taschenrechner_Iteration_1/Program.cs
+++ Taschenrechner_Iteration_1/Program.cs
@@ -10,6 +10,24 @@ namespace Taschenrechner_Iteration_1
     {
         static void Main(string[] args)
         {
+            //Titel: Addieren
+            //Story: Als Benutzer möchte ich zwei Gleitkommazahlen eingeben, um die Summe berechnen zu lassen.
+
+            Console.Write("Bitte den ersten Summanden eingeben: ");
+            string ersterSummand = Console.ReadLine();
+            Console.Write("Bitte den zweiten Summanden eingeben: ");
+            string zweiterSummand = Console.ReadLine();
+
+            // Eingabe in Zahl umwandeln
+            float ersterSummandAlsZahl = Convert.ToSingle(ersterSummand);
+            float zweiterSummandAlsZahl = Convert.ToSingle(zweiterSummand);
+
+            // Berechnung
+            float summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+
+            // Ausgabe
+            Console.WriteLine("Die Summe ist: {0}", summe);
+            Console.ReadLine();
         }
     }
 }
