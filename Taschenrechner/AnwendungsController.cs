7527a52bd5e5810a8eb9be563690bfdb94cb584f
Michael Koch <m.koch@know-how-akademie.de>
2020-09-08 22:56:51 +0200

    AnwendungsController erstellt

---
+++ Taschenrechner/AnwendungsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner
+{
+    class AnwendungsController
+    {
+        private RechnerModel model;
+        private ConsoleView view;
+
+        public AnwendungsController(RechnerModel model, ConsoleView view)
+        {
+            this.model = model;
+            this.view = view;
+        }
+
+        public void Ausfuehren()
+        {
+            string ersteZahlAlsString = view.HohleZahlVomBenutzer();
+            string operation = view.HoleOperatorVomBenutzer();
+            string zweiteZahlAlsString = view.HohleZahlVomBenutzer();
+
+
+            // Wandle Text in Gleitkommazahl
+            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist
+            double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
+            double zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
+
+            // Berechnung ausführen
+
+            model.Berechne(ersteZahl, zweiteZahl, operation);
+
+
+            // Ausgabe
+            view.GibResultatAus(operation);
+            view.WarteAufEndeDurchBenutzer();
+        }
+    }
+}
--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -8,43 +8,13 @@ namespace Taschenrechner
 {
     class Program
     {
-        // METHODE DEFINIEREN (in 7 Schritten)
-        // (optional) Modifizierer Definieren
-        // Datentyp des Rückgabewertes definieren
-        // Methodenname definieren
-        // Runde Klammern an den Methodennamen anfügen
-        // Überlegen welche Parameter benötigt werden (Optional diese definieren)
-        // Geschweifte Klammern einfügen
-        // Methode implementieren (Anweisungen in den Methodenrumpf schreiben)
-
-        
-
         static void Main(string[] args)
-        {
-            
+        {           
             RechnerModel model = new RechnerModel();
             ConsoleView view = new ConsoleView(model);
-            string ersteZahlAlsString = view.HohleZahlVomBenutzer();
-            string operation = view.HoleOperatorVomBenutzer();
-            string zweiteZahlAlsString = view.HohleZahlVomBenutzer();
-            
+            AnwendungsController controller = new AnwendungsController(model, view);
 
-            // Wandle Text in Gleitkommazahl
-            // TODO: Auslagern in Methode, wenn Struktur umfangreicher geworden ist
-            double ersteZahl = Convert.ToDouble(ersteZahlAlsString);
-            double zweiteZahl = Convert.ToDouble(zweiteZahlAlsString);
-
-            // Berechnung ausführen
-            
-            model.Berechne(ersteZahl, zweiteZahl, operation);
-
-
-            // Ausgabe
-            view.GibResultatAus(operation);
-            view.WarteAufEndeDurchBenutzer();
-        }
-
-
-        
+            controller.Ausfuehren();
+        }       
     }
 }
